5200cdc570d5b814523459c81333e0871b2eb73f
MichaelLorimer <michael.lorimer@hotmail.co.uk>
2019-01-03 16:32:59 +0000

    03/01/18_ enemies
    prijectile + bouncy enemy

---
+++ BasicPlatformer/Assets/_Script/GenericJumpingEnemy.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericJumpingEnemy : MonoBehaviour
+{
+    public float PointA;
+    public float PointB;
+
+    public Rigidbody2D EnemyRB;        //Store the players Rigid Body (Set in the Inspector)
+    public Animator EnemyAnimator; //Store the Players Animator for transitions (Set in the Inspector)
+
+    public float moveSpeed;             //Players move speed (Set in Inspector)
+    public float JumpHeight;
+    public bool FaceRight;       //Check if Player is facing right (Change True/ False dependant on circumstance)
+    public bool IsGrounded;
+    public bool CanJump;
+
+    public float WaitFor = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        EnemyRB = GetComponent<Rigidbody2D>();     //Get the Players RigidBody Component
+        EnemyAnimator = GetComponent<Animator>();  //Get the Players Animator Component
+        IsGrounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (FaceRight == false)
+        {
+            Vector2 Pos = EnemyRB.transform.position; //Define new Vectror2 to temperarily store the players position 
+            Pos.x -= moveSpeed * Time.deltaTime;       //Add the new speed to the temp variable (*Time.deltaTime = same movement regardless of FPS)
+            EnemyRB.transform.position = Pos;         //set the players transform to equal the temp position variable because...
+                                                      //PlayerRB.transform.position cannot be added 
+            if (EnemyRB.position.x <= PointA)
+            {
+                FaceRight = true;
+            }
+        }
+        if (FaceRight == true)
+        {
+            Vector2 Pos = EnemyRB.transform.position; //Define new Vectror2 to temperarily store the players position 
+            Pos.x += moveSpeed * Time.deltaTime;       //Add the new speed to the temp variable (*Time.deltaTime = same movement regardless of FPS)
+            EnemyRB.transform.position = Pos;         //set the players transform to equal the temp position variable because...
+                                                      //PlayerRB.transform.position cannot be added 
+            if (EnemyRB.position.x >= PointB)
+            {
+                FaceRight = false;
+            }
+        }
+
+        if(IsGrounded == true)
+        {
+            float SpeedStore = moveSpeed;
+            CanJump = true;
+
+            moveSpeed = 0;
+            WaitFor -= Time.deltaTime;
+
+            if (WaitFor <= 0f)
+            {
+                moveSpeed = 3;
+                WaitFor = 1f;
+                IsGrounded = false;
+
+            }
+        }
+
+        if (IsGrounded == false)
+        {
+            if (CanJump == true)
+            {
+                EnemyRB.AddForce(new Vector2(0, JumpHeight), ForceMode2D.Impulse);
+                CanJump = false;
+            }
+        }
+
+
+        /*if (FaceRight == true)
+        {
+            Vector2 Pos = EnemyRB.transform.position; //Define new Vectror2 to temperarily store the players position 
+            Pos.x -= moveSpeed * Time.deltaTime;       //Add the new speed to the temp variable (*Time.deltaTime = same movement regardless of FPS)
+            FaceRight = true;                         //Set RaceRight to false to flip the sprite 
+            EnemyRB.transform.position = Pos;         //set the players transform to equal the temp position variable because...
+                                                       //PlayerRB.transform.position cannot be added 
+        }
+        if (FaceRight == false)
+        {
+            Vector2 Pos = EnemyRB.transform.position; //Define new Vectror2 to temperarily store the players position 
+            Pos.x -= moveSpeed * Time.deltaTime;       //Add the new speed to the temp variable (*Time.deltaTime = same movement regardless of FPS)
+            FaceRight = false;                         //Set RaceRight to false to flip the sprite 
+            EnemyRB.transform.position = Pos;         //set the players transform to equal the temp position variable because...
+                                                       //PlayerRB.transform.position cannot be added 
+        }*/
+        FlipSprite();
+    }
+
+    void FlipSprite()
+    {
+        if (FaceRight == true)
+        {
+            transform.localScale = new Vector2(-1, 1); //If Right: flip to the Right = (x1, y1)
+        }
+        if (FaceRight == false)
+        {
+            transform.localScale = new Vector2(1, 1); //If Left: flip to the left = (x-1, y1)
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            IsGrounded = true;
+        }
+    }
+}
---
+++ BasicPlatformer/Assets/_Script/ProjectileScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    // ---------- Generic Projectile Script ------
+    //--------------------------------------------
+    //
+    // -- This Script is Designed to wprk wit hthe previouse Scripts made
+    // -- its primary use is for fast prototyping 
+    //
+    // -- Notes: 
+    // ----- For Graviity change objects RigidBody2D to Dynamic
+    //
+
+    // ---- Chached objects ----
+    //
+
+    public Rigidbody2D ProjectileBod; // Store Projectile RibidBody2D
+    GameObject Self;                  // Store Projectiles own GameObject
+    
+    // ---- Object Variables ----
+    //
+
+    public float MoveSpeed;          // How fast the Object moves (Set in Inpector)
+    public float LiveFor;            // How long the object will live for (Set in Inspector)
+
+    //-- Bools to determine shooting direction, can be combines for diagonal movement
+    //
+
+    public bool ShootLeft;           //Bool to determine if moving Left
+    public bool ShootRight;          //Bool to determine if moving Right
+    public bool ShootUp;             //Bool to determine if moving Up
+    public bool ShootDown;           //Bool to determine if moving Down
+
+    public bool UseTiming;           //Bool to determine if the object needs to die overtime
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ProjectileBod = GetComponent<Rigidbody2D>(); //Get and store the Projectiles Rigid Body
+        Self = this.gameObject;                      //Get and store Projectiles Own Game Object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShootLeft == true)
+        {
+            Vector2 Pos = ProjectileBod.transform.position; //Define new Vectror2 to temperarily store the Objects position 
+            Pos.x -= MoveSpeed * Time.deltaTime;            //Add the new speed to the temp variable (*Time.deltaTime = same movement regardless of FPS)
+            ProjectileBod.transform.position = Pos;         //set the Objects transform to equal the temp position variable because
+        }
+        if (ShootRight == true)
+        {
+            Vector2 Pos = ProjectileBod.transform.position; //Define new Vectror2 to temperarily store the Objects position 
+            Pos.x += MoveSpeed * Time.deltaTime;            //Add the new speed to the temp variable (*Time.deltaTime = same movement regardless of FPS)
+            ProjectileBod.transform.position = Pos;         //set the players transform to equal the temp position variable because //PlayerRB.transform.position cannot be added 
+        }
+        if (ShootUp == true)
+        {
+            Vector2 Pos = ProjectileBod.transform.position; //Define new Vectror2 to temperarily store the Objects position 
+            Pos.y += MoveSpeed * Time.deltaTime;       //Add the new speed to the temp variable (*Time.deltaTime = same movement regardless of FPS)
+            ProjectileBod.transform.position = Pos;         //set the players transform to equal the temp position variable because...
+                                                      //PlayerRB.transform.position cannot be added 
+        }
+        if (ShootDown == true)
+        {
+            Vector2 Pos = ProjectileBod.transform.position; //Define new Vectror2 to temperarily store the Objects position 
+            Pos.y -= MoveSpeed * Time.deltaTime;            //Add the new speed to the temp variable (*Time.deltaTime = same movement regardless of FPS)
+            ProjectileBod.transform.position = Pos;         //set the 10.96.16.200 transform to equal the temp position variable because
+        }
+
+        // -- Kill Object after countdown
+        if (UseTiming == true)
+        {
+            DeathTime();
+        }
+    }
+
+    void DeathTime()
+    {
+        if (LiveFor > 0f)
+        {
+            LiveFor -= Time.deltaTime;
+        }
+        else if (LiveFor <= 0f)
+        {
+            Destroy(Self);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(Self);
+    }
+
+}
